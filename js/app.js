2935cdae0d0192bb9355ce26b0c38cd3e9cf060f
Andres  Palacio <apalacio@uniempresarial.edu.co>
2021-10-05 16:28:36 -0500

    Modificacion de ruta SW y SW

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
+//validar el ambiente de trabajo (desarrollo/produccion)
+var url=window.location.href;
+var swLocation='/twittor/sw.js';
+
 if(navigator.serviceWorker){
-    navigator.serviceWorker.register('/sw.js')
+    if(url.includes('localhost')){
+        swLocation='/sw.js';
+    }
+    navigator.serviceWorker.register(swLocation)
 }
 
 
